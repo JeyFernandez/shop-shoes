3be3a662e84cb569a23310dd6badac7b934d8563
Jasson Fernandez <jeicobfer@gmail.com>
2023-12-08 23:09:54 -0600

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './screen/Home';
+import Contact from './screen/Contact';
 import './App.css';
+
+function Navbar() {
+  return (
+    <nav className='nav'>
+      <Link className='Link' to="/">Home</Link>
+      <Link className='Link' to="/contact">Contact</Link>
+    </nav>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <header className='container-nav'>
+          <h1 className='title'>Shoes Shop</h1>
+          <Navbar />
+        </header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/screen/Contact.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div>Welcome to contact</div>
+  )
+}
+
+export default Contact
